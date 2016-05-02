8cc0ddc4ac269e4e1cb15d0e502b03861f67474a
luxiaotong <luxiaotong713@sina.com>
2016-05-02 11:31:09 +0800

    1032/1032

--- 7_ReverseInteger/reverse_integer.c
+++ 7_ReverseInteger/reverse_integer.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <limits.h>
 #include "reverse_integer.h"
 int reverse(int x) {
     int remainder = 0, quotient = 0;
     int number = 0;
     int i = 0;
     int res = 0;
+    int tmp = 0;
     int arr[12] = {};
     while( x != 0 ) {
         remainder = x % 10;
@@ -17,6 +19,15 @@ int reverse(int x) {
     }
 
     for ( i = 0; i < number; i++ ) {
+        if ( res > 0 ) {
+            if ( res > (INT_MAX/10) ) {
+                return 0;
+            }
+        } else if ( res < 0 ) {
+            if ( res < (INT_MIN/10) ) {
+                return 0;
+            }
+        }
         res = res * 10 + arr[i];
     }
 
