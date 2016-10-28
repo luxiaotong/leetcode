e99d7c54f8c8a83481c1166248d809c0106ac48c
逯晓瞳 <shannon@luxiaotongdeMacBook-Pro-2.local>
2016-10-28 10:13:19 +0800

    Finish 282 6ms

--- 283_Move_Zeroes/main.c
+++ 283_Move_Zeroes/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 void moveZeroes(int* nums, int numsSize);
+void moveZeroes2(int* nums, int numsSize);
     
 int main() {
     int arr[] = {0, 1, 0, 3, 12};
     int len = sizeof(arr)/sizeof(int);
-    moveZeroes(arr, len);
+    moveZeroes2(arr, len);
     for ( int i = 0; i < len; i ++ ) {
         printf("%d ", arr[i]);
     }
@@ -28,6 +29,27 @@ void moveZeroes(int* nums, int numsSize) {
             if ( j == numsSize ) {
                 break;
             }
+        }
+    }
+
+    return;    
+}
+
+void moveZeroes2(int* nums, int numsSize) {
+    int i = 0, j = 0;
+    for ( i = 0, j = 1; j < numsSize; ) {
+        if ( nums[i] == 0 ) {
+            if ( nums[j] != 0 ) {
+                int tmp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = tmp;
+                continue;
+            } else {
+                j ++;
+            }
+        } else {
+            i ++;
+            j ++;
         }
     }
 
