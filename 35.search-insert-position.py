736c695384d387d043c7ad05cc504083efbaf19a
luxiaotong <luxiaotong713@sina.com>
2019-10-29 17:06:45 +0800

    35.search-insert-position binary-search

--- 35.search-insert-position.py
+++ 35.search-insert-position.py
@@ -7,20 +7,40 @@ from typing import List
 
 # @lc code=start
 class Solution:
+    # def searchInsert(self, nums: List[int], target: int) -> int:
+    #     index = -1
+    #     for i in range(len(nums)):
+    #         if nums[i] == target:
+    #             return i
+    #         elif nums[i] < target:
+    #             index = i
+    #         else:
+    #             return i
+    #     return len(nums)
+    
+    #binary-search
     def searchInsert(self, nums: List[int], target: int) -> int:
-        index = -1
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
-            elif nums[i] < target:
-                index = i
+        start = 0
+        end = len(nums)-1
+        while start <= end:
+            mid = (start + end)//2
+            
+            print(start, end , mid)
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                start = mid+1
             else:
-                return i
-        return len(nums)
+                end = mid-1
+
+        if nums[mid] > target:
+            return mid
+        else:
+            return mid+1
 # @lc code=end
 
 s = Solution()
-print("Input: [1,3,5,6], 5, Output:", s.searchInsert([1,3,5,6], 5))
-print("Input: [1,3,5,6], 2, Output:", s.searchInsert([1,3,5,6], 2))
-print("Input: [1,3,5,6], 7, Output:", s.searchInsert([1,3,5,6], 7))
-print("Input: [1,3,5,6], 0, Output:", s.searchInsert([1,3,5,6], 0))
+print("Input: [1,3,5,6], 5, \nOutput:", s.searchInsert([1,3,5,6], 5))
+print("Input: [1,3,5,6], 2, \nOutput:", s.searchInsert([1,3,5,6], 2))
+print("Input: [1,3,5,6], 7, \nOutput:", s.searchInsert([1,3,5,6], 7))
+print("Input: [1,3,5,6], 0, \nOutput:", s.searchInsert([1,3,5,6], 0))
