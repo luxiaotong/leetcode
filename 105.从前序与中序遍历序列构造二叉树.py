e219317932f6e78ffca2dec042b195aec8dd0406
luxiaotong <luxiaotong713@sina.com>
2020-01-13 14:36:25 +0800

    105.从前序与中序遍历序列构造二叉树.py

---
+++ 105.从前序与中序遍历序列构造二叉树.py
@@ -0,0 +1,57 @@
+#
+# @lc app=leetcode.cn id=105 lang=python3
+#
+# [105] 从前序与中序遍历序列构造二叉树
+#
+# https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+#
+# algorithms
+# Medium (63.30%)
+# Likes:    297
+# Dislikes: 0
+# Total Accepted:    36.1K
+# Total Submissions: 57K
+# Testcase Example:  '[3,9,20,15,7]\n[9,3,15,20,7]'
+#
+# 根据一棵树的前序遍历与中序遍历构造二叉树。
+# 
+# 注意:
+# 你可以假设树中没有重复的元素。
+# 
+# 例如，给出
+# 
+# 前序遍历 preorder = [3,9,20,15,7]
+# 中序遍历 inorder = [9,3,15,20,7]
+# 
+# 返回如下的二叉树：
+# 
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        # print("preorder:", preorder)
+        if len(preorder) == 0:
+            return None
+        mid = preorder[0]
+        idx = inorder.index(mid)
+        # print("mid, idx:", mid, idx)
+        root = TreeNode(mid)
+        root.left = self.buildTree(preorder[1:idx+1], inorder[:idx])
+        root.right = self.buildTree(preorder[idx+1:], inorder[idx+1:])
+        return root
+# @lc code=end
+
